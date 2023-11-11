2d40d52162169b59b5f53b572c33ea2cb85a27e3
engrab <engrbilawalhussain@gmail.com>
2023-11-11 09:23:27 +0500

    Adding topAppbar in Material 2

--- app/src/main/java/com/example/demoapp/MainActivity.kt
+++ app/src/main/java/com/example/demoapp/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.demoapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.android.material.appbar.MaterialToolbar
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var topAppBar: MaterialToolbar
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        topAppBar = findViewById(R.id.topAppbar)
+
+        topAppBar.setOnMenuItemClickListener {
+
+            when(it.itemId){
+                R.id.action_setting -> {
+                    Toast.makeText(this, "Setting is clicked", Toast.LENGTH_LONG).show()
+
+                    true
+                }
+
+                else -> { false}
+            }
+        }
     }
 }
